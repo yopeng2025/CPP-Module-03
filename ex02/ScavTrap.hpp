2e866b332abd9177c93f6712d1b89aa47c4453ad
yopeng <189126364+yopeng2025@users.noreply.github.com>
2026-03-05 11:10:29 +0100

    Add copy constructor and assignment operator

--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -9,6 +9,8 @@ class ScavTrap: public ClapTrap
     public:
         ScavTrap();
         ScavTrap(std::string name);
+        ScavTrap(const ScavTrap& other);
+        ScavTrap& operator=(const ScavTrap& other);
         ~ScavTrap();
         
         void    attack(const std::string& enemy);
